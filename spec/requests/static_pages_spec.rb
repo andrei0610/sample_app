7c69747c549f46dc7587f921bd47a3652720ffd8
Andrei Constantinescu <andrei.c@live.com>
2014-02-10 20:17:16 +0200

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,22 @@ describe "Static pages" do
     
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
+    
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
@@ -43,17 +59,17 @@ describe "Static pages" do
   end
   
   it "should have the right links on the layout" do
-      visit root_path
-      click_link "About"
-      expect(page).to have_title(full_title('About'))
-      click_link "Help"
-      expect(page).to have_title(full_title('Help'))
-      click_link "Contact"
-      expect(page).to have_title(full_title('Contact'))
-      click_link "Home"
-      click_link "Sign up now!"
-      expect(page).to have_title(full_title('Sign up'))
-      click_link "sample app"
-      expect(page).to have_title(full_title(''))
-    end
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
 end
